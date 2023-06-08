7bb8322533e4607f1159ade15cb5f1132dcf4bba
Jayden Bailey <jaydenkieran@gmail.com>
2023-06-08 17:43:09 +0100

    Add missing dark styles for error boundary

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -15,8 +15,8 @@ export default function Error({
 
   return (
     <div className={'w-full h-screen flex items-center justify-center bg-deepgray-500'}>
-      <div className={'bg-tile p-10 mx-2 max-w-lg rounded shadow-lg text-center border-t-4 border-orange-400'}>
-        <h2 className={'font-serif font-bold text-lg text-orange-400'}>There was an error</h2>
+      <div className={'bg-tile dark:bg-dark-500 p-10 mx-2 max-w-lg rounded shadow-lg text-center border-t-4 border-orange-400 dark:border-orange-300'}>
+        <h2 className={'font-serif font-bold text-lg text-orange-400 dark:text-orange-300'}>There was an error</h2>
         <p className={'text-sm'}>If this continues, please report it to us on <a href={'https://discord.gg/runescapewiki'} target={'_blank'} rel={'noreferrer'}>Discord</a>.</p>
         <div className={'mt-4'}>
           <button
