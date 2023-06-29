2129b2c60f2b8e70bd99defed01b76ddda65d034
Jayden Bailey <jaydenkieran@gmail.com>
2023-06-29 13:21:10 +0100

    Partially revert b0321b9
    Changing how the NumberInput works caused some regressions

--- src/app/components/generic/NumberInput.tsx
+++ src/app/components/generic/NumberInput.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 interface NumberInputProps extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'onChange'> {
   onChange?: (v: number) => void;
-  value?: number;
+  value: number;
 }
 
 const NumberInput: React.FC<NumberInputProps> = (props) => {
+  const [value, setValue] = useState<string>(props.value.toString() || '');
   const {onChange} = props;
+
+  useEffect(() => {
+    // If a new value is passed in as a prop, update the state of this component
+    setValue(props.value.toString() || '');
+  }, [props.value]);
 
   return (
     <input
       type={'number'}
       className={'form-control w-16'}
       {...props}
-      defaultValue={props.value}
+      value={value}
       onChange={(evt) => {
+        const valueAsNum = evt.currentTarget.valueAsNumber;
+        // Always update the value of this component locally
+        setValue(evt.currentTarget.value);
+
         // If it's a valid number (passes local HTML validation), call our onChange
         if (evt.currentTarget.validity.valid && onChange) {
-          onChange(evt.currentTarget.valueAsNumber);
+          onChange(valueAsNum);
         }
       }}
     />
