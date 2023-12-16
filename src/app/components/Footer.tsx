31ed7383bb3ed07ddbad5473c0402763ead1b4b0
Jayden Bailey <jaydenkieran@gmail.com>
2023-12-16 17:30:21 +0000

    Footer bg should match header bg

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className={'border-t-8 border-body-500 dark:border-dark-300 py-4 px-4 items-center text-xs bg-btns-300 dark:bg-dark-600 text-body-300 dark:text-gray-300'}>
+    <div className={'border-t-8 border-body-500 dark:border-dark-300 py-4 px-4 items-center text-xs bg-btns-400 dark:bg-dark-500 text-body-300 dark:text-gray-300'}>
       <div className={'max-w-7xl mx-auto flex gap-4 justify-between flex-wrap'}>
         <div>
           &copy; {new Date().getFullYear()} <a href={'https://weirdgloop.org'}>Weird Gloop</a> &#183;{' '}
