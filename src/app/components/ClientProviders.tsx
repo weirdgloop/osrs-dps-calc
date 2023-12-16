f8ea66ae8ca758e50ede67977b5be7d810a7ec69
Jayden Bailey <jaydenkieran@gmail.com>
2023-12-16 17:05:38 +0000

    Actually place footer at the bottom of the container

--- src/app/components/ClientProviders.tsx
+++ src/app/components/ClientProviders.tsx
@@ -16,12 +16,17 @@ const ClientProviders: React.FC<PropsWithChildren> = (props) => {
   return (
     <ThemeProvider enableSystem={true} attribute={'class'}>
       <StoreProvider store={store}>
-        <main>
-          <div className={'bg-pink-500 py-2 px-6 border-b border-pink-300 text-sm text-pink-100 select-none'}>
-            <span className={'font-bold mr-4'}>WORK IN PROGRESS</span> We&apos;re still working on this. It should not currently be used for actual calculation.
+        <main className={'flex flex-col h-[100vh]'}>
+          <div>
+            <div className={'bg-pink-500 py-2 px-6 border-b border-pink-300 text-sm text-pink-100 select-none'}>
+              <span className={'font-bold mr-4'}>WORK IN PROGRESS</span> We&apos;re still working on this. It should not
+              currently be used for actual calculation.
+            </div>
+            <TopBar/>
           </div>
-          <TopBar />
-          {children}
+          <div className={'flex-grow'}>
+            {children}
+          </div>
           <Footer />
         </main>
       </StoreProvider>
