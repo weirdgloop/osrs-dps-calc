197abee7ff2b555eac2945d0c8fac125dbee0df4
Jayden Bailey <jaydenkieran@gmail.com>
2024-01-10 04:07:31 +0000

    add manual calculate from equipment button to bonuses in advanced mode
    fixes #35

--- src/app/components/player/Bonuses.tsx
+++ src/app/components/player/Bonuses.tsx
@@ -7,10 +7,18 @@ import OtherBonuses from "@/app/components/player/bonuses/OtherBonuses";
 
 const Bonuses: React.FC = observer(() => {
   const store = useStore();
+  const {advancedMode} = store.prefs;
 
   return (
     <div className={'px-4 my-4'}>
-      <h4 className={'font-serif font-bold'}>Bonuses</h4>
+      <div className={'flex justify-between items-center gap-2'}>
+        <h4 className={'font-serif font-bold'}>Bonuses</h4>
+        {advancedMode && (
+          <button className={'text-xs underline'} onClick={() => store.recalculateEquipmentBonusesFromGear()}>
+            Calculate from equipment
+          </button>
+        )}
+      </div>
       <div className={'py-1'}>
         <div className={'flex gap-4 justify-center'}>
           <Offensive/>
