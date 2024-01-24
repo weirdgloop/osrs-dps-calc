d9cbf862e4d49e25746d6e88d20fb0423d902e2b
Jayden Bailey <jaydenkieran@gmail.com>
2024-01-24 15:59:10 +0000

    ui: adjust monster container padding + select position

--- src/app/components/monster/MonsterContainer.tsx
+++ src/app/components/monster/MonsterContainer.tsx
@@ -236,12 +236,9 @@ const MonsterContainer: React.FC = observer(() => {
   }, [toJS(loadouts), toJS(monster)]);
 
   return (
-    <div className="basis-4 flex flex-col grow md:grow-0">
-      <div className="px-2 md:px-0">
-        <MonsterSelect />
-      </div>
+    <div className="basis-4 flex flex-col grow mt-3 md:grow-0">
       <div
-        className="bg-tile dark:bg-dark-300 mt-2 sm:rounded-lg text-black dark:text-white shadow-lg"
+        className="bg-tile dark:bg-dark-300 sm:rounded-lg mt-6 text-black dark:text-white shadow-lg"
       >
         <div
           className="px-6 py-2 border-b-body-400 dark:border-b-dark-200 border-b md:rounded md:rounded-bl-none md:rounded-br-none flex justify-between items-center bg-body-100 dark:bg-dark-400"
@@ -274,8 +271,11 @@ const MonsterContainer: React.FC = observer(() => {
             </a>
           )}
         </div>
-        <div className="py-6 px-4">
+        <div className="py-4 px-4">
           <div className="mb-4">
+            <MonsterSelect />
+          </div>
+          <div>
             <div className="flex gap-8 flex-wrap justify-center">
               <div className="w-72">
                 <div className="flex gap-4">
