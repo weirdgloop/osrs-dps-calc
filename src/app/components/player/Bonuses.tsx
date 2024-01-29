334b58e3cd3760a3151538be18de638de2fa7302
LlemonDuck <napkinorton@gmail.com>
2024-01-29 09:10:25 -0500

    manual mode: fix unequal equipment bonus highlighting
    Another thing missed when the way we compute equipment stats was changed. `store.equipmentBonuses.{...}` now is always equal to `player.{...}`

--- src/app/components/player/Bonuses.tsx
+++ src/app/components/player/Bonuses.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@/state';
 import Offensive from '@/app/components/player/bonuses/Offensive';
 import Defensive from '@/app/components/player/bonuses/Defensive';
 import OtherBonuses from '@/app/components/player/bonuses/OtherBonuses';
+import { toJS } from 'mobx';
+import { calculateEquipmentBonusesFromGear } from '@/lib/Equipment';
 
 const Bonuses: React.FC = observer(() => {
   const store = useStore();
   const { manualMode } = store.prefs;
+
+  const player = toJS(store.player);
+  const monster = toJS(store.monster);
+  const computedStats = useMemo(() => calculateEquipmentBonusesFromGear(player, monster), [monster, player]);
 
   return (
     <div className="px-4 my-4">
@@ -21,9 +27,9 @@ const Bonuses: React.FC = observer(() => {
       </div>
       <div className="py-1">
         <div className="flex gap-4 justify-center">
-          <Offensive />
-          <Defensive />
-          <OtherBonuses />
+          <Offensive computedStats={computedStats} />
+          <Defensive computedStats={computedStats} />
+          <OtherBonuses computedStats={computedStats} />
         </div>
       </div>
     </div>
