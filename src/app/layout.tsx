4c7133e082d3575a85b94108958690a9185824b7
Jayden Bailey <jaydenkieran@gmail.com>
2024-02-04 23:29:33 +0000

    housekeeping: fix next metadata warning, split env files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,16 +6,12 @@ import { ibm, pts } from '@/fonts';
 import ClientProviders from '@/app/components/ClientProviders';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || ''),
   title: 'DPS Calculator - OSRS Wiki',
   description: "See how your gear stacks up against Old School RuneScape's monsters using the OSRS Wiki's damage-per-second calculator.",
   authors: [{ name: 'OSRS Wiki', url: 'https://oldschool.runescape.wiki' }],
   keywords: ['osrs', 'old school runescape', 'runescape', 'dps calculator', 'osrs dps', 'osrs wiki', 'osrs wiki dps'],
-  alternates: { canonical: 'https://tools.runescape.wiki/osrs-dps' },
-  openGraph: {
-    images: [
-      'https://tools.runescape.wiki/static/dps-logo.png',
-    ],
-  },
+  alternates: { canonical: `${process.env.NEXT_PUBLIC_BASE_URL}${process.env.NEXT_PUBLIC_BASE_PATH}` },
   twitter: {
     card: 'summary',
   },
