3685c9c172999096994ebaacb80f3649eca51551
Jayden Bailey <jaydenkieran@gmail.com>
2024-04-17 17:17:02 +0100

    Cleanup callback hell in InitialLoad.tsx dummy component

--- src/app/components/InitialLoad.tsx
+++ src/app/components/InitialLoad.tsx
@@ -17,40 +17,50 @@ const InitialLoad: React.FC = observer(() => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
+    const doFirstLoad = async () => {
+      const id = searchParams.get('id');
+      if (id) {
+        // If there was a share ID provided, load the data for it into the calculator
+        await store.loadShortlink(id);
+        window.history.replaceState({}, '', process.env.NEXT_PUBLIC_BASE_PATH);
+      } else {
+        // Else, try to load any previously saved session from localStorage
+        try {
+          const state = await localforage.getItem('dps-calc-state');
+          store.updateImportedData(state as ImportableData);
+        } catch (e) {
+          // If state couldn't be loaded (or there wasn't any), then try to load the username and lookup stats, at least
+          try {
+            const username = await localforage.getItem('dps-calc-username');
+            store.updateUIState({ username: username as string });
+            if (username) {
+              store.fetchCurrentPlayerSkills();
+            }
+          } catch (e2) { /* do nothing */ }
+        }
+
+        store.startStorageUpdater();
+      }
+
+      const monster = searchParams.get('monster');
+      if (monster) {
+        const target = store.availableMonsters.find((m) => m.id === parseInt(monster));
+        if (target) {
+          // If a monster ID was provided, set the active monster to it.
+          store.updateMonster(target);
+        }
+      }
+
+      setLoaded(true);
+    };
+
+    if (loaded) return;
+    doFirstLoad();
+
     // This code is necessary because searchParams may change later, when we strip the query params from the URL
     if (loaded) return;
     setLoaded(true);
 
-    const id = searchParams.get('id');
-    if (id) {
-      // If there was a share ID provided, load the data for it into the calculator
-      store.loadShortlink(id).then(() => {
-        window.history.replaceState({}, '', process.env.NEXT_PUBLIC_BASE_PATH);
-      });
-    } else {
-      // Else...
-      // Load any previously saved session from localStorage
-      localforage.getItem('dps-calc-state')
-        .then((s) => store.updateImportedData(s as ImportableData))
-        .catch(() => {})
-        // Also load username from browser storage if there is one and lookup stats
-        .finally(() => localforage.getItem('dps-calc-username')
-          .then((u) => {
-            store.updateUIState({ username: u as string });
-            if (u) store.fetchCurrentPlayerSkills();
-          })
-          .catch(() => {})
-          .finally(() => store.startStorageUpdater()));
-    }
-
-    const monster = searchParams.get('monster');
-    if (monster) {
-      const target = store.availableMonsters.find((m) => m.id === parseInt(monster));
-      if (target) {
-        // If a monster ID was provided, set the active monster to it.
-        store.updateMonster(target);
-      }
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams]);
 
