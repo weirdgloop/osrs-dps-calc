47a4fc7fc1df4c2e2ca0b7984e8ec6f17dfce0c4
Eric White <eric.white.austin@gmail.com>
2024-04-25 23:23:49 -0500

    Fix comment for Harralander tar.

--- src/lib/Equipment.ts
+++ src/lib/Equipment.ts
@@ -145,7 +145,7 @@ const ammoForRangedWeapons: { [weapon: number]: number[] } = {
   10149: [10142], // Swamp lizard, Guam tar
   10146: [10143], // Orange salamander, Marrentill tar
   10147: [10144], // Red salamander, Tarromin tar
-  10148: [10145], // Black salamander, Guam tar
+  10148: [10145], // Black salamander, Harralander tar
   28834: [28837], // Tecu salamander, Irit tar
   28869: [28872, 28878], // Hunters' sunlight crossbow
   29000: [28991], // Eclipse atlatl
