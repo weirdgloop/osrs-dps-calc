16c80a4494ab729c262424ece29b56f50c689448
LlemonDuck <napkinorton@gmail.com>
2024-06-04 13:29:51 -0400

    updated tob em scaling
    https://oldschool.runescape.wiki/w/Update:Theatre_of_Blood:_Entry_Mode_Improvements#Foe_Hitpoint_Scaling

--- src/lib/scaling/TheatreOfBlood.ts
+++ src/lib/scaling/TheatreOfBlood.ts
@@ -1,5 +1,14 @@
 import { Monster } from '@/types/Monster';
 import { TOB_EM_MONSTER_IDS, TOB_MONSTER_IDS } from '@/lib/constants';
+import { Factor } from '@/lib/Math';
+
+const ENTRY_MODE_SCALING: { [partySize: number]: Factor } = {
+  1: [10, 40],
+  2: [19, 40],
+  3: [27, 40],
+  4: [34, 40],
+  5: [40, 40],
+};
 
 const applyTobScaling = (m: Monster): Monster => {
   const { inputs } = m;
@@ -18,11 +27,12 @@ const applyTobScaling = (m: Monster): Monster => {
 
   if (TOB_EM_MONSTER_IDS.includes(m.id)) {
     const partySize = Math.min(5, Math.max(1, inputs.partySize));
+    const factor = ENTRY_MODE_SCALING[partySize];
     return {
       ...m,
       skills: {
         ...m.skills,
-        hp: Math.trunc(m.skills.hp * partySize / 5),
+        hp: Math.trunc(m.skills.hp * factor[0] / factor[1]),
       },
     };
   }
