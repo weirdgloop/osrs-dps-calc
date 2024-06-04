9c3082e0d8cd2203bf13c2d51d7282995b9c1fbc
LlemonDuck <napkinorton@gmail.com>
2024-06-04 16:36:44 -0400

    reworked toa scaling
    Closes #158

--- src/lib/scaling/TombsOfAmascut.ts
+++ src/lib/scaling/TombsOfAmascut.ts
@@ -1,105 +1,5 @@
-import {
-  AKKHA_IDS,
-  AKKHA_SHADOW_IDS,
-  BABA_IDS,
-  KEPHRI_OVERLORD_IDS,
-  KEPHRI_SHIELDED_IDS,
-  KEPHRI_UNSHIELDED_IDS,
-  P2_WARDEN_IDS,
-  P3_WARDEN_IDS,
-  TOA_CORE_IDS,
-  TOA_OBELISK_IDS,
-  TOMBS_OF_AMASCUT_MONSTER_IDS,
-  TOMBS_OF_AMASCUT_PATH_MONSTER_IDS,
-  ZEBAK_IDS,
-} from '@/lib/constants';
+import { TOA_WARDEN_CORE_EJECTED_IDS, TOMBS_OF_AMASCUT_MONSTER_IDS, TOMBS_OF_AMASCUT_PATH_MONSTER_IDS } from '@/lib/constants';
 import { Monster } from '@/types/Monster';
-
-interface ToaScalingValues {
-  base: number,
-  factor: number,
-}
-
-const getToaScalingValues = (id: number): ToaScalingValues | undefined => {
-  if (AKKHA_IDS.includes(id)) {
-    return {
-      base: 40,
-      factor: 10,
-    };
-  }
-
-  if (AKKHA_SHADOW_IDS.includes(id)) {
-    return {
-      base: 14,
-      factor: 5,
-    };
-  }
-
-  if (BABA_IDS.includes(id)) {
-    return {
-      base: 38,
-      factor: 10,
-    };
-  }
-
-  if (KEPHRI_SHIELDED_IDS.includes(id)) {
-    return {
-      base: 15,
-      factor: 10,
-    };
-  }
-
-  if (KEPHRI_UNSHIELDED_IDS.includes(id)) {
-    return {
-      base: 16,
-      factor: 5,
-    };
-  }
-
-  if (KEPHRI_OVERLORD_IDS.includes(id)) {
-    return {
-      base: 40,
-      factor: 1,
-    };
-  }
-
-  if (ZEBAK_IDS.includes(id)) {
-    return {
-      base: 58,
-      factor: 10,
-    };
-  }
-
-  if (TOA_OBELISK_IDS.includes(id)) {
-    return {
-      base: 26,
-      factor: 10,
-    };
-  }
-
-  if (P2_WARDEN_IDS.includes(id)) {
-    return {
-      base: 28,
-      factor: 5,
-    };
-  }
-
-  if (TOA_CORE_IDS.includes(id)) {
-    return {
-      base: 450,
-      factor: 10,
-    };
-  }
-
-  if (P3_WARDEN_IDS.includes(id)) {
-    return {
-      base: 88,
-      factor: 10,
-    };
-  }
-
-  return undefined;
-};
 
 const applyToaScaling = (m: Monster): Monster => {
   const { inputs } = m;
@@ -109,39 +9,36 @@ const applyToaScaling = (m: Monster): Monster => {
     return m;
   }
 
-  const values = getToaScalingValues(m.id);
-  if (!values) {
-    // either doesn't scale or isn't implemented
-    return m;
+  let newHp = m.skills.hp;
+  if (TOA_WARDEN_CORE_EJECTED_IDS.includes(m.id)) {
+    newHp = 4500;
   }
 
-  const invoFactor = TOA_CORE_IDS.includes(m.id) ? inputs.toaInvocationLevel : 4 * inputs.toaInvocationLevel;
+  const invoFactor = Math.trunc((TOA_WARDEN_CORE_EJECTED_IDS.includes(m.id) ? 1 : 4) * inputs.toaInvocationLevel / 10);
+  newHp += Math.trunc(newHp * invoFactor / 100);
 
-  let pathLevelFactor = 0;
-  if (TOMBS_OF_AMASCUT_PATH_MONSTER_IDS.includes(m.id)) {
-    const pathLevel = Math.min(6, Math.max(0, inputs.toaPathLevel));
-    if (pathLevel >= 1) {
-      pathLevelFactor += 3;
-      pathLevelFactor += 5 * inputs.toaPathLevel;
-    }
+  const pathLevel = Math.min(6, Math.max(0, inputs.toaPathLevel));
+  if (TOMBS_OF_AMASCUT_PATH_MONSTER_IDS.includes(m.id) && pathLevel >= 1) {
+    const pathLevelFactor = 3 + 5 * inputs.toaPathLevel;
+    newHp = Math.trunc(newHp * (100 + pathLevelFactor) / 100);
   }
 
   const partySize = Math.min(8, Math.max(1, inputs.partySize));
-  let partyFactor = 0;
   if (partySize >= 2) {
-    partyFactor += 9 * Math.min(2, partySize - 1);
-  }
-  if (partySize >= 400) {
-    partyFactor += 6 * (partySize - 3);
+    let partyFactor = 9 * (partySize === 3 ? 2 : 1);
+    if (partySize >= 4) {
+      partyFactor += 6 * (partySize - 3);
+    }
+
+    newHp = Math.trunc(newHp * (10 + partyFactor) / 10);
   }
 
-  const newHp = Math.trunc(
-    values.base
-      * (1000 + invoFactor)
-      * (100 + pathLevelFactor)
-      * (10 + partyFactor)
-      / (1000 * 100 * 10),
-  ) * values.factor;
+  // some rounding, for once
+  if (newHp > 100) {
+    const roundTo = newHp > 300 ? 10 : 5;
+    newHp = Math.trunc((newHp + Math.trunc(roundTo / 2)) / roundTo) * roundTo;
+  }
+
   return {
     ...m,
     skills: {
