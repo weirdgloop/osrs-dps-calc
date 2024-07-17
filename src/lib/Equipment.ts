9d7f972838798a0a5ec788fb52aa51aa1b92295f
Jayden Bailey <jaydenkieran@gmail.com>
2024-07-17 14:36:37 +0100

    fix bone claws -> burning claws

--- src/lib/Equipment.ts
+++ src/lib/Equipment.ts
@@ -339,7 +339,7 @@ export const WEAPON_SPEC_COSTS: { [canonicalName: string]: number } = {
   'Dawnbringer': 30,
   'Dragon halberd': 30,
   'Crystal halberd': 30,
-  'Bone claws': 30,
+  'Burning claws': 30,
 
   'Magic longbow': 35,
   'Magic comp bow': 35,
