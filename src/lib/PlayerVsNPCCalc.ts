5433db61b9976e8a126841130d3ed18234f5dddd
jmyaeger <87620453+jmyaeger@users.noreply.github.com>
2024-08-19 10:44:26 -0400

    Fix custom monster toa defence scaling (#450)

--- src/lib/PlayerVsNPCCalc.ts
+++ src/lib/PlayerVsNPCCalc.ts
@@ -164,7 +164,9 @@ export default class PlayerVsNPCCalc extends BaseCalc {
     const statBonus = this.trackAdd(DetailKey.NPC_DEFENCE_STAT_BONUS, defenceStyle ? bonus : 0, 64);
     let defenceRoll = this.trackFactor(DetailKey.NPC_DEFENCE_ROLL_BASE, effectiveLevel, [statBonus, 1]);
 
-    if (TOMBS_OF_AMASCUT_MONSTER_IDS.includes(this.monster.id) && this.monster.inputs.toaInvocationLevel) {
+    const isCustomMonster = this.monster.id === -1;
+
+    if ((TOMBS_OF_AMASCUT_MONSTER_IDS.includes(this.monster.id) || isCustomMonster) && this.monster.inputs.toaInvocationLevel) {
       defenceRoll = this.track(DetailKey.NPC_DEFENCE_ROLL_TOA, Math.trunc(defenceRoll * (250 + this.monster.inputs.toaInvocationLevel) / 250));
     }
 
