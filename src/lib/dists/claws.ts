9584f4fc0d24c28e62249f6d3b16f0757b97aa09
Rhea <napkinorton@gmail.com>
2024-09-16 21:12:28 -0400

    burning claws: account for bugged burn hitsplat (#474)

--- src/lib/dists/claws.ts
+++ src/lib/dists/claws.ts
@@ -1,6 +1,7 @@
 import {
   AttackDistribution, HitDistribution, Hitsplat, WeightedHit,
 } from '@/lib/HitDist';
+import { sum } from 'd3-array';
 
 const generateTotals = (accRoll: number, totalRolls: number, acc: number, max: number, highOffset: number): [chance: number, low: number, high: number] => {
   const low = Math.trunc(max * (totalRolls - accRoll) / 4);
@@ -125,14 +126,44 @@ export const burningClawSpec = (acc: number, max: number): AttackDistribution =>
   return new AttackDistribution([dist]);
 };
 
+// truth table ; 1 = burn, 0 = no burn
+export const BURN_MATRIX = [
+  [0, 0, 0],
+  [0, 0, 1],
+  [0, 1, 0],
+  [0, 1, 1],
+  [1, 0, 0],
+  [1, 0, 1],
+  [1, 1, 0],
+  [1, 1, 1],
+];
+export const BURN_EXPECTED = [0, 1, 2].map((accRoll) => sum(BURN_MATRIX, (row) => {
+  const burnChance = 0.15 * (accRoll + 1);
+  const burn1 = (row[0] === 1 ? burnChance : (1 - burnChance));
+  const burn2 = (row[1] === 1 ? burnChance : (1 - burnChance));
+  const burn3 = (row[2] === 1 ? burnChance : (1 - burnChance));
+  const chanceOfRow = burn1 * burn2 * burn3;
+
+  let damage = sum(row, (entry) => entry * 10);
+  if (row[0] === 1 && row[1] === 1) {
+    // there's a (presumed) bug here, where if the first two hitsplats apply burn,
+    // then they overlap and miss 1 damage on the first tick.
+    // https://discord.com/channels/177206626514632704/1098698914498101368/1285181996896620566
+    damage -= 1;
+  }
+
+  return chanceOfRow * damage;
+}));
+
 export const burningClawDoT = (acc: number): number => {
   // 10 damage burn x3 hitsplats, 15/30/45% chance per splat dependent on which roll hits
   let accumulator = 0;
+
   for (let accRoll = 0; accRoll < 3; accRoll++) {
     const prevRollsFail = (1 - acc) ** accRoll;
     const thisRollHits = prevRollsFail * acc;
-    const burnChancePerSplat = 0.15 * (accRoll + 1);
-    accumulator += 30 * thisRollHits * burnChancePerSplat;
+
+    accumulator += thisRollHits * BURN_EXPECTED[accRoll];
   }
   return accumulator;
 };
