e08275719a383a9a89cd5b8869dec51b7e6a860d
Jonathan Yaeger <jmyaeger@gmail.com>
2026-02-15 16:16:56 -0500

    Fix ayak spec attack speed

--- src/lib/PlayerVsNPCCalc.ts
+++ src/lib/PlayerVsNPCCalc.ts
@@ -1913,6 +1913,10 @@ export default class PlayerVsNPCCalc extends BaseCalc {
       return this.getAttackSpeed() - 1;
     }
 
+    if (this.opts.usingSpecialAttack && this.wearing('Eye of ayak')) {
+      return 5;
+    }
+
     return this.getAttackSpeed();
   }
 
