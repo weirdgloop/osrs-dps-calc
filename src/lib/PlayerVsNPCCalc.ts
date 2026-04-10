59062e41b795655f6873f6f9a89a3fe156d7c0ff
Orson Peters <orsonpeters@gmail.com>
2026-04-10 21:55:14 +0200

    Reorder crystal blessing dmg/acc% for magic and ensure powered staff (#811)
    Reorder crystal dmg/acc% for magic and ensure powered staff

--- src/lib/PlayerVsNPCCalc.ts
+++ src/lib/PlayerVsNPCCalc.ts
@@ -854,7 +854,9 @@ export default class PlayerVsNPCCalc extends BaseCalc {
     const baseRoll = effectiveLevel * (magicBonus + 64);
     let attackRoll = baseRoll;
 
-    if (this.wearing('Crystal blessing') && this.player.spell === null) {
+    const usingPoweredStaff = this.player.equipment.weapon?.category === EquipmentCategory.POWERED_STAFF
+      && this.player.style.stance !== 'Manual Cast';
+    if (usingPoweredStaff && this.wearing('Crystal blessing')) {
       const crystalPieces = (this.wearing('Crystal helm') ? 1 : 0) + (this.wearing('Crystal legs') ? 2 : 0) + (this.wearing('Crystal body') ? 3 : 0);
       attackRoll = Math.trunc(attackRoll * (20 + crystalPieces) / 20);
     }
@@ -959,11 +961,6 @@ export default class PlayerVsNPCCalc extends BaseCalc {
     const mattrs = this.monster.attributes;
     const { buffs } = this.player;
 
-    if (this.wearing('Crystal blessing') && spell === null) {
-      const crystalPieces = (this.wearing('Crystal helm') ? 1 : 0) + (this.wearing('Crystal legs') ? 2 : 0) + (this.wearing('Crystal body') ? 3 : 0);
-      maxHit = Math.trunc(maxHit * (40 + crystalPieces) / 40);
-    }
-
     if (spell) {
       maxHit = getSpellMaxHit(spell, magicLevel);
       if (spell?.name === 'Magic Dart') {
@@ -1065,6 +1062,13 @@ export default class PlayerVsNPCCalc extends BaseCalc {
     }
 
     maxHit = this.trackAddFactor(DetailKey.MAX_HIT_MAGIC_DMG, maxHit, [magicDmgBonus, 1000]);
+
+    const usingPoweredStaff = this.player.equipment.weapon?.category === EquipmentCategory.POWERED_STAFF
+      && this.player.style.stance !== 'Manual Cast';
+    if (usingPoweredStaff && this.wearing('Crystal blessing')) {
+      const crystalPieces = (this.wearing('Crystal helm') ? 1 : 0) + (this.wearing('Crystal legs') ? 2 : 0) + (this.wearing('Crystal body') ? 3 : 0);
+      maxHit = Math.trunc(maxHit * (40 + crystalPieces) / 40);
+    }
 
     if (blackMaskBonus) {
       maxHit = Math.trunc(maxHit * 23 / 20);
